8e258a1095ab30225fd85479db40baa6518c084f
mananispiwpiw <rafijaeger@gmail.com>
2023-11-24 18:07:58 +0700

    add handler for task

--- main.go
+++ main.go
@@ -6,30 +6,36 @@ import (
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter,r *http.Request){
-	if r.URL.Path != "/hello"{
-		http.Error(w,"404 Not Found",http.StatusNotFound)
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
 		return
 	}
-	if r.Method != "GET"{
-		http.Error(w,"Mehod is not supported",http.StatusMethodNotAllowed)
+	if r.Method != "GET" {
+		http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Fprintf(w,"Hello Rafi")
+	fmt.Fprintf(w, "Hello Rafi")
 }
 
-func taskHandler(){
-
+func taskHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err : %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful ")
+	task := r.FormValue("task")
+	fmt.Fprintf(w, "Task = %s\n", task)
 }
 
-func main(){
+func main() {
 	fileServer := http.FileServer(http.Dir("./views"))
-	http.Handle("/",fileServer)
-	//http.HandleFunc("/task",taskHandler)
-	http.HandleFunc("/hello",helloHandler)
+	http.Handle("/", fileServer)
+	http.HandleFunc("/task", taskHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Server start at port : 8080")
-	if err := http.ListenAndServe(":8080",nil);err!=nil{
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
